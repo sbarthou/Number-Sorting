6cb826c26b04147997f288ba21fc47488e32d61d
sbarthou <sebastianbarthou@gmail.com>
2023-05-01 03:32:04 -0400

    commit

--- test1.py
+++ test1.py
@@ -1,6 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-num = [7, 4, 3, 1, 8, 9, 2, 0, 6, 5]
-snum = np.zeros(len(num))
+numbers = [7, 4, 3, 1, 8, 9, 2, 0, 6, 5]
+
+def ordenado(lista):
+    i = lista[0]
+    for j in range(1, len(lista)):
+        if i > lista[j]:
+            return False
+        else:
+            return True
+
+while not ordenado(numbers):
+    for i in range(len(numbers) - 1):
+        a = numbers[i] 
+        b = numbers[i+1]
+        if a > b:
+            numbers[i] = b
+            numbers[i+1] = a
+    
+# for i in range(len(numbers) - 1):
+#     a = numbers[i] 
+#     b = numbers[i+1]
+#     if a > b:
+#         numbers[i] = b
+#         numbers[i+1] = a
+
+print(numbers)
