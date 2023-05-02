725cc077eb5f37ce20beac958f3a18e572bbddf3
sbarthou <sebastianbarthou@gmail.com>
2023-05-02 00:45:22 -0400

    final test1

---
+++ test 1/test1.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+"""
+Este código revisa para cada elemento (a) en una lista si el siguiente elemento (b) es menor (if a > b) mediante un ciclo for, 
+si lo es mueve el elemento (a) una posición adelante y al elemento (b) una hacia atrás. El ciclo for se ejecuta las veces necesarias mediante un ciclo while.
+"""
+
+
+size = int(input("Cantidad de números: "))   # solicitar cantidad de números
+numbers = np.arange(size)   # generar la cantidad de números deaseados desde el 0 hasta size - 1
+np.random.shuffle(numbers)   # desordenar números generados
+zeros = np.zeros(size)   # array de ceros de tamaño size para generar gráfico
+
+def ordenado(lista):   # función para deteriminar si los números están ordenados (True or False)
+    i = 1
+    while i < len(lista):
+        if lista[i] < lista[i - 1]:
+          return False
+        i += 1
+    return True
+
+n = 0   # variable para contar el número de ciclos realizados
+while not ordenado(numbers):
+    for i in range(len(numbers) - 1):
+        a = numbers[i] 
+        b = numbers[i+1]
+        if a > b:
+            numbers[i] = b
+            numbers[i+1] = a
+    n += 1
+
+print(numbers, n)
